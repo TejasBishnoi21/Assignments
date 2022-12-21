6d0eb1efde6aa3b0cd8fd0fff247163a395d5164
TejasBishnoi21 <tejas.vishnoi@gmail.com>
2022-12-21 13:34:56 +0530

    counter_211-2

---
+++ 211-2/counter/src/components/Counter.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+
+const Counter = ()=>{
+    const [count, setCount] = useState(0);
+
+    function add(){
+        setCount((prev)=>prev+1)
+    };
+    
+    const subs = ()=>{
+        setCount((prev)=>prev-1)
+    }
+    const double = ()=>{
+        setCount((prev)=>prev*2)
+    }
+
+    return <>
+        <div>
+            <h2 data-testid='counter-header'>Counter</h2>
+            <h3 data-testid='count'>{count}</h3>
+            <button data-testid='add-btn' onClick={()=>add()}>+</button>
+            <button disabled={count <=0} data-testid='subtract-btn' onClick={()=>subs()}>-</button>
+            <button data-testid='double-btn' onClick={()=>double()}>Double</button>
+
+            <div>
+                <button onClick={()=> setCount(0)}>Reset</button>
+            </div>
+
+        </div>
+    </>
+}
+
+export default Counter;
