b8b36d7dba55888505426adabc284dfd20c09587
TejasBishnoi21 <tejas.vishnoi@gmail.com>
2023-01-30 00:25:35 +0530

    all added

--- reg/NEM111/FileSys/index.js
+++ reg/NEM111/FileSys/index.js
@@ -1,6 +1,70 @@
-const fls = require('fs');
+const argvs = process.argv;
+const argv = argvs.slice(2);
+// console.log(argv)
+const operation = argv[0]
+const a = argv[1];
+const b = argv[2];
 
-fls.readFile('./text.txt',{encoding:'utf-8'}, (err, data)=>{
-    if(err) console.log(err)
-    else console.log(data)
-})
+if(operation === "read"){
+    const fs=require("fs");
+
+    fs.readFile(`${a}`,{encoding:"utf-8"},(err,data)=>{
+    if(err){
+    console.log("Cannot read the file")
+    console.log(err)
+    } else {
+    console.log(data)
+    }
+    })
+    
+}
+
+if(operation === "append"){
+    const fs=require("fs");
+    
+    fs.appendFile(`${b}`,`${a}\n`,(err)=>{
+    if(err){
+    console.log("Cannot read the file")
+    console.log(err)
+    } 
+    })
+}
+
+if(operation === "create"){
+    const fs=require("fs");
+    
+    fs.writeFile(`./${a}`,b,(err)=>{
+    if(err){
+    console.log("Cannot create file")
+    console.log(err)
+    }
+    })
+}
+if(operation === "delete"){
+    const fs=require("fs");
+     fs.unlinkSync(`./${a}`)
+}
+
+if(operation === "rename"){
+    const fs=require("fs");
+    
+    fs.rename(`${a}`,`${b}`,(err)=>{
+    if(err){
+    console.log("Cannot create file")
+    console.log(err)
+    }
+    })
+}
+if(operation === "list"){
+    const fs=require("fs");
+    
+    fs.readdir(`${a}`, (err, files)=>{
+    if(err){
+    console.log(err)
+    }
+    files.forEach((file)=>{
+        console.log(file); 
+      });
+    })
+}
+
